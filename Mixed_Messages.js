c7c455044c5c9f9c6af425210207a1a62416d3c5
Noel Freid <noelfreid@Noels-MacBook-Air.local>
2021-06-21 19:32:32 +0300

    First commit

---
+++ Mixed_Messages.js
@@ -0,0 +1,18 @@
+let randomNum = Math.floor(Math.random()*5); // creating a number between 0-4
+switch(randomNum){ // now we display the message according to the number
+    case 0:
+        console.log('Which bear is the most condescending? A pan-duh!');
+        break;
+    case 1:
+        console.log('What kind of noise does a witch’s vehicle make? Brrrroooom, brrroooom.');
+        break;
+    case 2:
+        console.log('What’s brown and sticky? A stick.');
+        break;
+    case 3:
+        console.log('Why are elevator jokes so classic and good? They work on many levels.');
+        break;
+    case 4:
+        console.log('What did the police officer say to his belly-button? You’re under a vest.');
+        break;
+} 
